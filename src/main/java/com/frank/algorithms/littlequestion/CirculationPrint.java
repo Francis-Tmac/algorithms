af30b5fcf72f727454d788a90946807f2525f644
FrancisTmac <FrancisTmac@163.com>
2021-02-26 08:29:53 +0800

    add circulationPrint

---
+++ src/main/java/com/frank/algorithms/littlequestion/CirculationPrint.java
@@ -0,0 +1,114 @@
+package com.frank.algorithms.littlequestion;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @program: algorithms
+ * @description:
+ * @author: Francis-Tmac
+ * @create: 2020-12-25
+ **/
+
+public class CirculationPrint {
+
+    public static void main(String[] args) {
+        Circulation circulation = new Circulation();
+        new Thread(()->{
+            circulation.printA(3);
+        }).start();
+        new Thread(()->{
+            circulation.printB(3);
+        }).start();
+        new Thread(()->{
+            circulation.printC(3);
+        }).start();
+    }
+}
+
+class Circulation{
+    Lock lock = new ReentrantLock();
+
+    Condition conditionA = lock.newCondition();
+    Condition conditionB = lock.newCondition();
+    Condition conditionC = lock.newCondition();
+
+    volatile int flag = 1;
+
+    public void printA(int count){
+        lock.lock();
+        try{
+            while (true){
+                if(flag != 1){
+                    conditionA.await();
+                }
+                System.out.println("A");
+                TimeUnit.SECONDS.sleep(1);
+                flag = 2;
+
+                conditionB.signal();
+                if(count <= 1){
+                    break;
+                }
+                count--;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally{
+            lock.unlock();
+        }
+    }
+
+    public void printB(int count){
+        lock.lock();
+        try{
+            while (true){
+                if(flag != 2){
+                    conditionB.await();
+                }
+                System.out.println("B");
+                TimeUnit.SECONDS.sleep(1);
+                flag = 3;
+
+                conditionC.signal();
+                if(count <= 1){
+                    break;
+                }
+                count--;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally{
+            lock.unlock();
+        }
+    }
+
+    public void printC(int count){
+        lock.lock();
+        try{
+            while (true){
+                if(flag != 3){
+                    conditionC.await();
+                }
+                System.out.println("C");
+                TimeUnit.SECONDS.sleep(1);
+                flag = 1;
+
+                conditionA.signal();
+                if(count <= 1){
+                    break;
+                }
+                count--;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally{
+            lock.unlock();
+        }
+    }
+}
