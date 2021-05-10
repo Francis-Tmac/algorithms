0d3795bcdee3e0783ab6a2d56c7d6a35ba422817
frank.yang@yingzt.com <frank.yang@yingzt.com>
2021-05-10 09:53:46 +0800

    aop aop

--- src/test/java/AOPTest.java
+++ src/test/java/AOPTest.java
@@ -9,15 +9,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @Date 2021/5/9
  * @Author frank
  * @Description:
+ *
+ * 1. 传入配置类，创建IOC 容器
+ * 2. 注册配置类，调用refresh() 刷新容器
+ * 3. registerBeanPostProcessors(beanFactory); 注册bean 的后置处理器拦截bean的创建
+ *
  */
 public class AOPTest {
-
     @Test
     public void test_1(){
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
                 MainConfigOfAOP.class);
         MathCalculator mathCalculator = applicationContext.getBean(MathCalculator.class);
         mathCalculator.div(4,2);
-
     }
 }
